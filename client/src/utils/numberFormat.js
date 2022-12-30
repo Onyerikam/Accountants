b807a2ea9fa1f9b8246503b9dce65b99dc006e82
Onyerikam <115038141+Onyerikam@users.noreply.github.com>
2022-12-30 10:51:12 +0100

    Delete client dir

--- client/src/utils/numberFormat.js
+++
@@ -1,9 +0,0 @@
-export function seperator(numb) {
-    var str = numb.toString().split(".");
-    str[0] = str[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-    return str.join(".");
-}
-
-export function round(numb){
-    return Math.round((numb) * 100) / 100;
-}
