a90e3054e90c30446498e58af953a41b7ac44c8f
Onyerikam <115038141+Onyerikam@users.noreply.github.com>
2022-12-30 20:39:45 +0100

    Update rest.js

--- client/src/Api/rest.js
+++ client/src/Api/rest.js
@@ -1,4 +1,4 @@
-const BASE_URL = "https://accountants.onrender.com/"
+const BASE_URL = "https://accountants.onrender.com"
 
 export const post = async (method, url, data = null) => {
     const URL = `${BASE_URL}/${url}`;
